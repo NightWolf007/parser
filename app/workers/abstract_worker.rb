de09cc4b45b65079c491fb9bbfdd1e3aa2a18469
NightWolf007 <dimon19960121@gmail.com>
2015-12-20 04:14:54 +0300

    Fixed update items

--- app/workers/abstract_worker.rb
+++ app/workers/abstract_worker.rb
@@ -4,10 +4,15 @@ class AbstractWorker
   include Sidekiq::Worker
 
   def perform(url, partner_id)
-    self.items(document(open(url))).each do |item|
-      item[:id] = SecureRandom.hex 10
-      item[:partner_id] = partner_id
-      Item.create item
+    self.items(document(open(url))).each do |item_params|
+      old_item = Item.find_by partner_id: partner_id, partner_item_id: item_params[:partner_item_id]
+      if old_item
+        old_item.update_attributes item_params
+      else
+        item_params[:id] = SecureRandom.hex 10
+        item_params[:partner_id] = partner_id
+        Item.create item_params
+      end
     end
   end
 
